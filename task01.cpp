b541f727343d85d558f38fafb94a28b50a811f71
anosha-zahid-123 <anoshazahid129@gmail.com>
2024-10-13 11:19:09 +0500

    first task on github

---
+++ task01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1=10;
+    int num2=20;
+    int sum = num1+num2;
+    cout<<sum<<endl;
+    int sub = num1-num2;
+    cout<<sub<<endl;
+    int mult = num1*num2;
+    cout<<mult<<endl;
+    float decimal=num1/num2;
+    cout<<decimal<<endl;
+    int modulus = num1%num2;
+    cout<<modulus<<endl;
+return 0;
+}
